8ad6d59de5a0d596012f2a91b9c4cc1dd2c19fb8
cic7860-creator <cic7860@gmail.com>
2026-07-15 11:48:27 +0900

    Update models.py

--- models.py
+++ models.py
@@ -2,28 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Dispatch(db.Model):
-    __tablename__ = 'dispatch'
-    
+# 💡 [신규] 센터 정보를 저장하는 테이블
+class Center(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    delivery_date = db.Column(db.Date, nullable=False)
-    center_address = db.Column(db.String(200), nullable=True) 
-    vehicle_num = db.Column(db.String(20), nullable=False)
-    driver_name = db.Column(db.String(50), nullable=False)
-    store_code = db.Column(db.String(50), nullable=True)
-    store_name = db.Column(db.String(100), nullable=False)
-    store_address = db.Column(db.String(200), nullable=True)
-    delivery_seq = db.Column(db.Integer, default=1)
-    buffer_time = db.Column(db.Integer, default=15)
-    
-    # 💡 [신규] 카카오내비 연동을 위한 GPS 좌표 저장 컬럼
-    store_x = db.Column(db.String(50), nullable=True) # 경도
-    store_y = db.Column(db.String(50), nullable=True) # 위도
-    
-    center_depart_time = db.Column(db.DateTime, nullable=True)
-    estimated_arrival = db.Column(db.DateTime, nullable=True)
-    departure_time = db.Column(db.DateTime, nullable=True)
-    is_departed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    address = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return f"<Dispatch {self.driver_name} -> {self.store_name}>"
+# 기존 배차 테이블 (센터명 컬럼 추가)
+class Dispatch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    delivery_date = db.Column(db.Date)
+    center_name = db.Column(db.String(100))     # 💡 신규: 센터명
+    center_address = db.Column(db.String(255))
+    vehicle_num = db.Column(db.String(50))
+    driver_name = db.Column(db.String(100))
+    store_code = db.Column(db.String(50))
+    store_name = db.Column(db.String(100))
+    store_address = db.Column(db.String(255))
+    delivery_seq = db.Column(db.Integer)
+    buffer_time = db.Column(db.Integer, default=10)
+    store_x = db.Column(db.String(50))
+    store_y = db.Column(db.String(50))
+    is_departed = db.Column(db.Boolean, default=False)
+    departure_time = db.Column(db.DateTime)
+    estimated_arrival = db.Column(db.DateTime)
+    center_depart_time = db.Column(db.DateTime)
